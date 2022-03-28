5c785ba2c8c2a7b14ebd68d7c03d42684982a1dc
Jessica13 <jessica.spera@insa-lyon.fr>
2022-03-28 08:14:58 +0200

    rm TD3

--- TD3/Q5/code/matEqualQ.h
+++
@@ -1,4 +0,0 @@
-#include <stdbool.h>
-#include "matrice.h"
-
-bool matEqualQ(int A[N][N], int B[N][N]);
--- TD3/Q5/code/utilMatrice.h
+++
@@ -1,4 +0,0 @@
-#include "matrice.h"
-
-int lireMatrice(FILE *fich,int mat[N][N]);
-int ecrireMatrice(FILE *fich,int mat[N][N]);
