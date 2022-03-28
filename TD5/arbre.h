cf674e82d08d28306844d9b20e49c02db50758f5
Jessica13 <jessica.spera@insa-lyon.fr>
2022-03-28 08:15:48 +0200

    rm TD4 et TD5

--- TD5/arbre.h
+++
@@ -1,6 +0,0 @@
-# include <stdlib.h>
-# include "arbre_type.h"
-
-ARBRE creerArbre(int val, ARBRE filsGauche, ARBRE filsDroit);
-int afficheArbre(ARBRE arbre);
-int hauteurArbre(ARBRE arbre);
--- TD5/arbre_type.h
+++
@@ -1,9 +0,0 @@
-struct model_noeud
-{
-    int val;
-    struct model_noeud *filsGauche;
-    struct model_noeud *filsDroit;
-};
-
-typedef struct model_noeud NOEUD;
-typedef NOEUD *ARBRE;
